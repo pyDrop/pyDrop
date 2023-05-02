24d07f9afb2e781dee85d574aa0a3234adf6f403
Tim Carlson <tim.carlson@gmail.com>
2023-05-01 21:35:13 -0600

    Add sphinx configuration

---
+++ sphinx/code.rst
@@ -0,0 +1,5 @@
+Code Documentation
+==================
+
+.. automodule:: pyDrop.exceptions.cg_exceptions
+    :members:
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -6,7 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyDrop Documentation'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'pyDrop'
 copyright = '2023, TC'
 author = 'TC'
 release = '0.0.1'
@@ -14,10 +18,14 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc','sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
 
 
 
